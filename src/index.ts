b418b2ebe21cdb777185a9bb1aac87eb328e39a0
khine-dev <khine.dev.9417@gmail.com>
2025-01-20 20:46:13 +0700

    browse service

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { auth_service_proxy, user_service_proxy } from "./services/auth_service";
 import cors from "cors";
+import { browse_service_proxy } from "./services/browse_service";
 require("dotenv").config();
 
 const PORT: Readonly<number> = 8000;
@@ -14,6 +15,7 @@ app.use(cors({
 
 app.use("/api/v1/auth", auth_service_proxy);
 app.use("/api/v1/users", user_service_proxy);
+app.use("/api/v1/browse", browse_service_proxy);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello world from burmese chit chat api gateway");
