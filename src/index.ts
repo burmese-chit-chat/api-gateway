14fe6950228bbfad8290655a23ae8ad934787de9
khine-dev <khine.dev.9417@gmail.com>
2025-02-13 03:53:15 +0700

    health check and noti

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import { browse_service_proxy } from "./services/browse_service";
 import cron from "node-cron";
 import keep_alive from "./helpers/keep_alive";
 import { chat_service_proxy } from "./services/chat_service";
+import { notification_service_proxy } from "./services/notification_service";
 require("dotenv").config();
 
-cron.schedule("*/2 * * * *", keep_alive);
+// cron.schedule("*/10 * * * *", keep_alive);
 
 const PORT: Readonly<number> = 8000;
 const app = express();
@@ -23,9 +24,14 @@ app.use("/api/v1/auth", auth_service_proxy);
 app.use("/api/v1/users", user_service_proxy);
 app.use("/api/v1/browse", browse_service_proxy);
 app.use("/api/v1/chat", chat_service_proxy);
+app.use("/api/v1/notifications", notification_service_proxy);
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello world from burmese chit chat api gateway");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
 });
 
 app.listen(PORT, () => {
